36c6b6abad98b1e3d59ca1b5ce146dabf535a2a2
Ajeet Thakur <ajeetthakur@Ajeets-MacBook-Pro.local>
2018-11-02 12:03:13 +0300

    Code modified to fix some stupid issues.

--- src/app/panels/country-panel/country-panel.component.ts
+++ src/app/panels/country-panel/country-panel.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
-import { Country } from '../../view-models/Country';
+import { Country } from "../../view-models/Country";
 
 @Component({
-  selector: 'app-country-panel',
-  templateUrl: './country-panel.component.html',
-  styleUrls: ['./country-panel.component.css']
+  selector: "app-country-panel",
+  templateUrl: "./country-panel.component.html",
+  styleUrls: ["./country-panel.component.css"]
 })
 export class CountryPanelComponent implements OnInit {
+  @Input()
+  country: Country;
+  @Input()
+  index = 1;
 
-  @Input() country: Country;
-  @Input() index = 1;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
